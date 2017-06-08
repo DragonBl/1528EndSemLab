99d441aea36a68e9326c1d8f2770452dc0e235b5
DragonBl <vinaykumar777777@gmail.com>
2017-06-08 11:04:52 +0530

    Tv implement

---
+++ src/Tv.java
@@ -0,0 +1,52 @@
+
+
+public class Tv implements ControlInterface {
+	
+		boolean power = false;
+		private int volume;
+		private int channel;
+		
+		public Tv(int volume, int channel) {
+			this.volume = volume;
+			this.channel = channel;
+		}
+		
+		public Tv(){};
+		
+		
+		@Override
+		public boolean powerOnOff() {
+			if (power == true)
+			{
+				power = false;
+			}
+			else if(power == false)
+			{
+				power = true;
+			}
+			return power;
+		}
+		
+		@Override
+		public int ToggleUp() {
+			if (channel+1 == 100)
+			{
+				channel = 0;
+				return channel;
+			}
+			else
+				return ++channel;
+			
+		}
+		
+		public int ToggleDown() {
+			if (channel-1 == -1)
+			{
+				channel = 99;
+				return channel;
+			}
+			else
+				return --channel;
+			
+		}
+}
