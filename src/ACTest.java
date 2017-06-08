fe60ca0768a030fc9b243101c3540fc87fb43133
DragonBl <vinaykumar777777@gmail.com>
2017-06-08 12:10:43 +0530

    First Junit Test

---
+++ src/ACTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ACTest {
+	
+	AC ac;
+	RemoteControl rm;
+	boolean test;
+
+	@Before
+	public void setUp() throws Exception {
+		ac = new AC(35);
+	}
+
+	@Test
+	public void testPowerOnOff() {
+		//fail("Not yet implemented");
+		test = true;
+		assertEquals(test,ac.powerOnOff());
+	}
+
+	@Test
+	public void testToggleUp() {
+		//fail("Not yet implemented");
+		int x = 35;
+		assertEquals(x,ac.ToggleUp());
+	}
+
+	@Test
+	public void testToggleDown() {
+		//fail("Not yet implemented");
+		int z = 15;
+		assertEquals(z,ac.ToggleDown());
+	}
+
+}
